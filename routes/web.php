a41349a77401d0baf9d9978c37eebc65911618e0
henvisic <henvisi_1994@hotmail.com>
2022-07-11 22:28:17 -0500

    vista de correo

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\emailPruebaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/prueba/{email}', [emailPruebaController::class, 'enviar_email']);
